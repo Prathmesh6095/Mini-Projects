35064055cfcb7200af6a82e5c1eb975c2e3738bc
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-27 14:38:30 +0530

    syntax error fixed

--- guessno/main.py
+++ guessno/main.py
@@ -4,12 +4,13 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input(f"Guess a nnumber between 1 and {x}")
+        guess = int(input(f"Guess a nnumber between 1 and {x} "))
         if guess < random_number:
             print('Sorry! guess again. Too low');
         elif guess > random_number:
             print('Sorry! guess again. Too big')
 
     print(f"Yay, Congrats! you guessed the {random_number} correctly.")
+
 
 guess(10) 
