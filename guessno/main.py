372fc2e156968ed575896e65f7ac98f68e21ac2e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-03-27 12:43:28 +0530

    take input

--- guessno/main.py
+++ guessno/main.py
@@ -4,4 +4,6 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        
+        guess = input(f"Guess a nnumber between 1 and {x}")
+        if guess < random_number:
+            print('Sorry! guess again')
