a8181eec81636321a3d8c24d2a66ada9be49cdaa
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-01 13:35:11 +0530

    conditional statements add

--- guessno/main.py
+++ guessno/main.py
@@ -16,8 +16,11 @@ def computer_guess(x):
     low = 1
     high = x
     feedback = ''
-    while feedback != 'c' and low != high:
-        guess = random.randint(low, high)
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low #could also be high b/c low = high
         feedback = input(f"Is {guess} too high (H), too low(L), or correct (c)??")
         if feedback == 'h':
             high = guess - 1
