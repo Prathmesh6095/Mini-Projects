0a8c7b740a60d9af8abd400597fbec86bd9480ff
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-07 10:03:25 +0530

    take a user input

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -9,4 +9,25 @@ def get_valid_word(words):
 
     return word
 
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set() #what the user has guessed
 
+    #getting user input
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - user_letter:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again.')
+    
+    else:
+        print('Invalid character. Please try again.')
+
+
+user_input = input("Type something: ")
+print(user_input)
