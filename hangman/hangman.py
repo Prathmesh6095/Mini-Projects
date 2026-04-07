c320809e2907c5f39beb79703d9169461012cd4b
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-07 10:19:08 +0530

    while loop used to maintain game until you win or lose

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,7 +3,7 @@ from words import words
 import string
 
 def get_valid_word(words):
-    word  = random.choice(words) #randomly chooses something from the list
+    word  = random.choice(words) # randomly chooses something from the list
     while '_' in word or ' ' in word:
         word = random.choice(words)
 
@@ -13,21 +13,28 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word) # letters in the word
     alphabet = set(string.ascii_lowercase)
-    used_letters = set() #what the user has guessed
+    used_letters = set() # what the user has guessed
 
-    #getting user input
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - user_letter:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a','b','cd']) --> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again.')
-    
-    else:
-        print('Invalid character. Please try again.')
+        # what current word is (i.e W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in  word]
+        print('Current word: ',' '.join(word_list))
 
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
-user_input = input("Type something: ")
-print(user_input)
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again.')
+        
+        else:
+            print('Invalid character. Please try again.')
+
+hangman()
