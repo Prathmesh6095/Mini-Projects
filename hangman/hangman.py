c46106b7b8d536bc187edefc844a25ca388c3466
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-04-07 09:51:27 +0530

    add function to take a random word from words module

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,5 +2,11 @@ import random
 from words import words
 import string
 
+def get_valid_word(words):
+    word  = random.choice(words) #randomly chooses something from the list
+    while '_' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
 
 
